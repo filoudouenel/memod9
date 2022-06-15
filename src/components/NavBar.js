3a3c7a7d23fb85c6d9c4364ea641e4ee4ec85bce
Yvan Douënel <y.douenel@coopernet.fr>
2022-06-15 11:19:55 +0200

    Ajout du lien de menu /others-tables

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
     <ul className="list-unstyled d-flex justify-content-center mt-5">
       <li className="m-2"><Link to="/">Accueil</Link></li>
       <li className="m-2"><Link to="/my-tables">Mes tableaux</Link></li>
+      <li className="m-2"><Link to="/others-tables">Les tableaux des autres</Link></li>
       <li className="m-2"><Link to="/about">A propos de l'application</Link></li>
     </ul>
   );
