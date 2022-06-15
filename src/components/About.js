45037ee9fa6236eb6e368cc1875bfe8bb4e917dc
Yvan Douënel <y.douenel@coopernet.fr>
2022-06-15 10:24:22 +0200

    text about up.

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 const About = () => {
     return (
-        <div>
-            <p>Integer urna mi, finibus sit amet augue vel, lobortis egestas dui. Quisque nec tristique tortor. Etiam felis orci, tincidunt eget eros eu, auctor accumsan diam. Duis ac leo nec lectus auctor sodales volutpat sit amet nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi scelerisque, erat vitae tristique tristique, nulla nisi semper metus, quis finibus turpis nisl pretium magna. Nulla sem risus, volutpat commodo rutrum id, sodales eget eros. Etiam consectetur dignissim auctor. Quisque vel pellentesque justo, ut dictum justo. Nunc sodales, neque id condimentum scelerisque, diam diam luctus metus, ac hendrerit sapien dolor vitae ex. Quisque ultricies lectus blandit vulputate eleifend.
+        <div className="mt-5 about">
+            <h3 className="mb-5">Memopus dans l'avenir</h3>
+            <p>
+                Cette application à but pédagogique a été créée avec React du côté front et Drupal du côté back.
 
-Ut eget risus eget dui tristique bibendum vitae eget metus. Fusce porta a nulla sed malesuada. Sed nec viverra lorem. Mauris vulputate blandit placerat. Donec vel velit eu tellus laoreet luctus ut ut augue. Vestibulum viverra consectetur nisi sit amet feugiat. Mauris nec commodo ex, vitae bibendum diam. Mauris facilisis vehicula dolor, ac condimentum ex eleifend sed. Nullam eu convallis nulla, at pretium velit. Ut mollis eros tortor, ut semper nisl blandit ut. In id eros pharetra lacus condimentum commodo. Vivamus euismod, nunc eget tempor sollicitudin, lacus tortor suscipit felis, vel tincidunt eros mi non augue. Etiam maximus lacus nec dolor ornare, ut varius purus mollis.</p>
+                <br></br>Elle s’appuie sur la technique de mémorisation dite « <a target="_blank" href="https://ncase.me/remember/fr.html">Répétition espacée</a> ».
+
+                </p>
+                <p>
+                Elle mériterait d’être améliorée, consolidée et étendue.<br></br>
+                Pour ce faire, des ressources ont été trouvées et une nouvelle version devrait être diffusée en août 2023.
+                </p>
+                <p>L'objectif de cette application est d'aider un maximum d'apprenants de façon libre et gratuite. S'il n'est pas possible de faire autrement, des annonceurs apparaîtront peut être.</p>
+                <p>Si vous avez des idées ou des ressources pour l’amélioration et le développement de cette application, contactez moi : y.douenel@coopernet.fr.
+                </p>
+                <p className="mt-5">
+                Yvan Douënel, <span className="grey-light">fondateur de Coopernet</span>
+            </p>
         </div>
     );
 }
