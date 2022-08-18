1ec26cf7fa634a402c88de8ba7891a961ae843a0
Yvan Douënel <y.douenel@coopernet.fr>
2022-08-18 11:51:07 +0200

    lien pour le mode administration activé seulement après identification.

--- src/components/App.js
+++ src/components/App.js
@@ -71,26 +71,29 @@ class App extends Component {
                 </h1>
               </div>
               <div className="col text-light">
-                {/* icône des paramètres (engrenage) */}
-                <div className={this.state.admin_mode ? "text-warning d-flex align-items-center gap-1" : "text-white d-flex align-items-center gap-1"}
-                  onClick={e => {
-                    this.handleAdministrationMode();
-                  }}>
-                  <svg
+                {this.state.is_logged_in && (
+                  <div className={this.state.admin_mode ? "text-warning d-flex align-items-center gap-1" : "text-white d-flex align-items-center gap-1"}
+                    onClick={e => {
+                      this.handleAdministrationMode();
+                    }}>
+                    {/* icône des paramètres (engrenage) */}
+                    <svg
 
-                    role="button"
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 14 16"
-                    height="1.5rem"
-                    width="1.5rem"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M14 8.77v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45-.69-1.92h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.23v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.75v.02zM7 11c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path>
-                  </svg>
-                  {this.state.admin_mode ? <span role="button">Désactiver le mode administration</span> : <span role="button">Activer le mode administration</span>}
-                </div>
+                      role="button"
+                      stroke="currentColor"
+                      fill="currentColor"
+                      strokeWidth="0"
+                      viewBox="0 0 14 16"
+                      height="1.5rem"
+                      width="1.5rem"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M14 8.77v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45-.69-1.92h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.23v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.75v.02zM7 11c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path>
+                    </svg>
+                    {this.state.admin_mode ? <span role="button">Désactiver le mode administration</span> : <span role="button">Activer le mode administration</span>}
+                  </div>
+                )}
+
                 {this.state.coopernet.url_server == 'https://coopernet.fr/' ? "" : (<div>{this.state.coopernet.url_server}</div>)}
               </div>
             </div>
